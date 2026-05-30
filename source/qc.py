35dd3b0e09d60ec9bfd6d1e729ff0a3a9edac91c
Alphative <Alphative85@gmail.com>
2026-05-30 02:56:26 +0200

    Fix argparse global scope, add mt_prefix parameter

--- source/qc.py
+++ source/qc.py
@@ -14,17 +14,6 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-#agrparse
-parser = argparse.ArgumentParser()
-parser.add_argument("--min_genes", type = int, default=200)
-parser.add_argument("--max_genes", type = int, default=2500)
-parser.add_argument("--max_pct_mt", type = float, default=5.0)
-parser.add_argument("--data_path", type = str, required=True)
-parser.add_argument("--output_path", type = str, required=True)
-parser.add_argument("--metadata_path", type = str, required=True)
-
-
-
 #func
 
 def load_data(data_path):
@@ -33,8 +22,8 @@ def load_data(data_path):
     logger.info(f"Before filtering: {adata.n_obs} ")
     return adata
 
-def compute_qc(adata):
-    adata.var["mt"] = adata.var_names.str.startswith("MT-")
+def compute_qc(adata, mt_prefix):
+    adata.var["mt"] = adata.var_names.str.startswith(mt_prefix)
     adata.var["ribo"] = adata.var_names.str.startswith(("RPS", "RPL"))
     adata.var["malat1"] = adata.var_names == "MALAT1"
     logger.info(f"Tracking mitochondrial genes: {adata.var['mt'].sum()}")
@@ -76,11 +65,25 @@ def save_metadata(adata_before, adata_after, args, output_path):
 
 #MAIN
 def main():
+    #agrparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--min_genes", type = int, default=200)
+    parser.add_argument("--max_genes", type = int, default=2500)
+    parser.add_argument("--max_pct_mt", type = float, default=5.0)
+    parser.add_argument("--data_path", type = str, required=True)
+    parser.add_argument("--output_path", type = str, required=True)
+    parser.add_argument("--metadata_path", type = str, required=True)
+    parser.add_argument("--mt_prefix", type = str, default="MT-") 
     args = parser.parse_args()
+    #loading data
     adata = load_data(data_path=args.data_path)
+    #filtering dead genes
     adata = filter_genes(adata)
-    cell_qc, gene_qc = compute_qc(adata)
+    #compute qc
+    cell_qc, gene_qc = compute_qc(adata,mt_prefix=args.mt_prefix)
+    #filtering
     adata_filtered = filter_cells(adata, cell_qc, min_genes=args.min_genes, max_genes=args.max_genes, max_pct_mt=args.max_pct_mt)
+    #saving
     save_results(adata_filtered, args.output_path)
     save_metadata(adata, adata_filtered, args, output_path=args.metadata_path)
 if __name__ == "__main__" :
