e24872a3364bd47ad03c297e731a93c6df1524dc
Alphative <Alphative85@gmail.com>
2026-05-31 23:35:19 +0200

    Add test_cluster.py

---
+++ tests/test_cluster.py
@@ -0,0 +1,49 @@
+#=========================
+#imports
+#=========================
+import sys
+import anndata
+import pytest
+import scanpy as sc
+import numpy as np
+
+sys.path.append("../source")
+from cluster import run_pca, run_neighbors, run_clustering, run_umap
+
+@pytest.fixture
+def sample_matrix():
+    return np.random.randint(0, 10, size=(30, 50))
+
+@pytest.fixture
+def sample_adata(sample_matrix):
+    adata = anndata.AnnData(X=sample_matrix)
+    return adata
+
+
+def test_run_pca(sample_adata):
+    n_comps=1
+    sample_adata = run_pca(sample_adata, n_comps=n_comps)
+    assert sample_adata.obsm["X_pca"].shape[1] == n_comps
+
+def test_run_neighbors(sample_adata):
+    n_neighbors=2
+    sample_adata = run_pca(sample_adata, n_comps=1)
+    sample_adata = run_neighbors(sample_adata, n_neighbors=n_neighbors)
+    assert "connectivities" in sample_adata.obsp
+
+def test_run_clustering(sample_adata):
+    n_comps=1
+    n_neighbors=2
+    resolution=0.5
+    sample_adata = run_pca(sample_adata, n_comps=n_comps)
+    sample_adata = run_neighbors(sample_adata, n_neighbors=n_neighbors)
+    sample_adata = run_clustering(sample_adata, resolution=resolution)
+    assert "leiden" in sample_adata.obs
+
+def test_run_umap(sample_adata):
+    n_comps=1
+    n_neighbors=2
+    sample_adata = run_pca(sample_adata, n_comps=n_comps)
+    sample_adata = run_neighbors(sample_adata, n_neighbors=n_neighbors)
+    sample_adata = run_umap(sample_adata)
+    assert "X_umap" in sample_adata.obsm
